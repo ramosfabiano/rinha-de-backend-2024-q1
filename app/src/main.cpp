60a88c0d5cf228bcc3b0e03638a9c903bf4ab811
ramosfabiano <ramos_fabiano@yahoo.com.br>
2024-02-17 20:12:55 -0300

    Reorganizing app configuration; Hello world!

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,45 +3,26 @@
 #include <drogon/drogon.h>
 
 using namespace drogon;
+using namespace drogon::orm;
 
 
-std::string getEnv_(std::string const & key) 
-{
-    char * val;
-    val = std::getenv(key.c_str());
-    return val == NULL ? std::string("") : std::string(val);
-}
+using Callback = std::function<void (const HttpResponsePtr &)> ;
 
 
 int main (int argc, const char **argv)
 {
-    const auto port = std::atoi(getEnv_("APP_PORT").c_str());
 
-    std::cout << "Listening at port " << port << std::endl;
+    app().loadConfigFile("/etc/rinha/config.json");
 
-    const auto postgresHost = getEnv_("POSTGRES_HOST");
-    const auto postgresPort = getEnv_("POSTGRES_PORT").c_str();
-    const auto postgresDBName = getEnv_("POSTGRES_DB");
-    const auto postgresUser = getEnv_("POSTGRES_USER");
-    const auto postgresPassword = getEnv_("POSTGRES_PASSWORD");
+    app().setLogLevel(trantor::Logger::kTrace);
 
-    std::cout << "Postgres Host: " << postgresHost << std::endl;
-    std::cout << "Postgres Port: " << postgresPort << std::endl;
-    std::cout << "Postgres DB Name: " << postgresDBName << std::endl;
-    std::cout << "Postgres User: " << postgresUser << std::endl;
-    std::cout << "Postgres Password: " << postgresPassword << std::endl;
-
-    std::cout << "Connecting to Postgres..." << std::endl;
-    // TODO: create db client
-
-
-    app()
-        .setLogPath("./")
-        .setLogLevel(trantor::Logger::kDebug)
-        .addListener("0.0.0.0", port)
-        .setThreadNum(8);
-
-    //app().registerController(controller);
+    app().registerHandler("/", [](const HttpRequestPtr& req, Callback &&callback)
+    {
+        auto resp = HttpResponse::newHttpResponse();
+        auto hostname = std::getenv("HOSTNAME");
+        resp->setBody("Hello World from " + std::string(hostname));
+        callback(resp);
+    });
 
     app().run();
 
