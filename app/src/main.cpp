b7569b8cdee0b06600c43b36ea68fce8df2fc040
ramosfabiano <ramos_fabiano@yahoo.com.br>
2024-02-17 17:51:28 -0300

    Main drogon loop.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <cstdlib>
-#include <thread>
-#include <chrono>
 #include <drogon/drogon.h>
 
 using namespace drogon;
 
 int main (int argc, const char **argv)
 {
-    // APP_PORT env
-    auto port = std::getenv("APP_PORT") ? std::atoi(std::getenv("APP_PORT")) : 8080;
+    const auto port = std::getenv("APP_PORT") ? std::atoi(std::getenv("APP_PORT")) : 8080;
+    std::cout << "Listening at port " << port << std::endl;
 
-    while(true)
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Listening at port " << port << std::endl;
-    }
+    app()
+        .setLogPath("./")
+        .setLogLevel(trantor::Logger::kDebug)
+        .addListener("0.0.0.0", port)
+        .setThreadNum(8);
+
+    //app().registerController(controller);
+
+    app().run();
+
+    std::cout << "Exiting." << std::endl;
 
     return 0;
 }
