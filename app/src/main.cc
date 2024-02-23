27f3d7007a4dc53beac7e0542d30ee4d1268da0f
ramosfabiano <ramos_fabiano@yahoo.com.br>
2024-02-23 15:14:32 -0300

    Setting app config options

--- app/src/main.cc
+++ app/src/main.cc
@@ -12,7 +12,7 @@ using namespace drogon::orm;
 using Callback = std::function<void(const HttpResponsePtr &)>;
 
 int main(int argc, const char **argv) {
-    // allow for docker-compose to properly start the database...
+    // allow for docker-compose to properly start the database/cache...
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
     app().loadConfigFile("/etc/rinha/config.json");
