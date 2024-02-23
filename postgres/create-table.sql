27009f9864277ce10e53b5093d57efa9dd35ddf2
ramosfabiano <ramos_fabiano@yahoo.com.br>
2024-02-23 08:49:38 -0300

    Improving queries

--- postgres/create-table.sql
+++ postgres/create-table.sql
@@ -10,10 +10,11 @@ CREATE TABLE IF NOT EXISTS clientes (
 CREATE TABLE IF NOT EXISTS transacoes (
   id SERIAL PRIMARY KEY,  
   client_id INTEGER NOT NULL REFERENCES clientes(id),
-  valor INTEGER NOT NULL CHECK (valor != 0),
+  valor INTEGER NOT NULL CHECK (valor > 0),
   tipo VARCHAR(1) NOT NULL CHECK (tipo IN ('c', 'd')),
   descricao VARCHAR(10) NOT NULL,
   realizada_em TIMESTAMPTZ DEFAULT (NOW())
+  --saldo_posterior INTEGER NOT NULL
 );
 
 CREATE INDEX transaction_client_index ON transacoes (client_id ASC);
@@ -25,25 +26,25 @@ CREATE INDEX transaction_date_index ON transacoes (realizada_em DESC);
 CREATE OR REPLACE FUNCTION processa_transacao()
 RETURNS TRIGGER AS $$
 BEGIN
-  IF NEW.valor = 0 THEN
+  IF NEW.valor <= 0 THEN
     RAISE EXCEPTION 'Valor inválido.';
   END IF;
+  IF NEW.tipo NOT IN ('c', 'd') THEN
+    RAISE EXCEPTION 'Tipo de transação inválido.';
+  END IF;  
   IF NEW.tipo = 'c' THEN
     UPDATE clientes
     SET saldo = saldo + NEW.valor
     WHERE id = NEW.client_id;
   ELSE 
-    IF NEW.tipo = 'd' THEN
-      IF (SELECT limite + saldo - NEW.valor FROM clientes WHERE id = NEW.client_id) < 0 THEN
-        RAISE EXCEPTION 'Saldo+Limite insuficientes.';
-      END IF;
-      UPDATE clientes
-      SET saldo = saldo - NEW.valor
-      WHERE id = NEW.client_id;
-    ELSE
-      RAISE EXCEPTION 'Tipo de transação inválido.';
+    IF (SELECT limite + saldo - NEW.valor FROM clientes WHERE id = NEW.client_id) < 0 THEN
+      RAISE EXCEPTION 'Saldo+Limite insuficientes.';
     END IF;
+    UPDATE clientes
+    SET saldo = saldo - NEW.valor
+    WHERE id = NEW.client_id;
   END IF;  
+  --NEW.saldo_posterior = (SELECT saldo FROM clientes WHERE id = NEW.client_id);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -62,8 +63,5 @@ BEGIN
   INSERT INTO clientes (limite, saldo) VALUES (1000000, 0);
   INSERT INTO clientes (limite, saldo) VALUES (10000000, 0);
   INSERT INTO clientes (limite, saldo) VALUES (500000, 0);
-
-  --INSERT INTO transacoes (client_id, valor, tipo, descricao) VALUES (1, 10, 'c', 'teste');
-  --INSERT INTO transacoes (client_id, valor, tipo, descricao) VALUES (1, 20, 'd', 'teste');
 END;
 $$;
